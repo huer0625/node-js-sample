9ce2ab26820d0db8158b3c1c4e7fd9203f9726fc
Ubuntu <ubuntu@ip-172-31-45-11.us-west-2.compute.internal>
2013-07-10 13:57:04 +0000

    to string

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
  // response.send('Hello World! 2');
   var buf = new Buffer(fs.readFileSync("index.html"));
-  response.send("hello, the world");
+  response.send(buf.toString());
   
 });
 
